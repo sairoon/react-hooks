f4bdc1e1a3e6f51c0705fa2a04b9a5328307b918
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-06-25 01:27:24 +0600

    multiple useState on same component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,33 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
- const [name, setName] = useState('')
- const handleChange = (e) => {
+  /*  const handleChange = (e) => {
    setName(e.target.value)
   // console.log(name); 
- }
+ } */
 
   return (
     <>
       <div className="container mx-auto mt-10">
-        <input onChange={handleChange} type="text" placeholder="Enter your text" />
-        <h1>{name}</h1>
+        <input
+          className="px-3 py-2"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          className="ms-2 px-3 py-2"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+        />
+        <h1>My name is: {name}</h1>
+        <h1>My email address is: {email}</h1>
       </div>
     </>
   );
