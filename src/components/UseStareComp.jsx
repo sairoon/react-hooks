c8955c90aaa3697ad7d491a45186c897b0a6352b
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-06-25 01:38:46 +0600

    useStare hook complete

---
+++ src/components/UseStareComp.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const UseStareComp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+  return (
+    <>
+      <div className="container mx-auto mt-10">
+        <input
+          className="px-3 py-2"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          className="ms-2 px-3 py-2"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+        />
+        <h1>My name is: {name}</h1>
+        <h1>My email address is: {email}</h1>
+      </div>
+    </>
+  )
+}
+
+export default UseStareComp
