67bb78e766f3cb00af76b9bf588e5717644de7e3
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-07-15 21:03:23 +0600

    count on title with useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
 import "./App.css";
 import UseEffectComp from "./components/UseEffectComp";
 import UseStareComp from "./components/UseStareComp";
 
 function App() {
- 
-
   return (
     <>
-    <UseStareComp/>
-    <UseEffectComp/>
+      <UseStareComp />
+      <UseEffectComp />
     </>
   );
 }
--- src/components/UseEffectComp.jsx
+++ src/components/UseEffectComp.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UseEffectComp = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
   return (
     <>
       <div className="container mx-auto mt-4 border border-white rounded-md">
         <div className="p-4">
           <h1 className="text-3xl font-bold mb-4">useEffect hook</h1>
+          <button className="px-3 py-1 border border-white rounded-md" onClick={() => setCount(count + 1)}>Click</button>
+          <p className="mt-4">You clicked {count} times</p>
         </div>
       </div>
     </>
